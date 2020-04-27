0480698c648d2ae14087b92887b003599846627d
oshinj <45084634+oshinj@users.noreply.github.com>
2020-04-26 17:56:59 -0700

    Updates

---
+++ precache-manifest.a9820ab7ed7e7d1b09f9e4514c6b9b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d7863c3473d382c6ac2d905bd364bd",
+    "url": "/dax_beauty/index.html"
+  },
+  {
+    "revision": "20f54036e58d6bfee745",
+    "url": "/dax_beauty/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16488550c24a004b8fc6",
+    "url": "/dax_beauty/static/js/2.7bee7a29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dax_beauty/static/js/2.7bee7a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f54036e58d6bfee745",
+    "url": "/dax_beauty/static/js/main.c752e8f5.chunk.js"
+  },
+  {
+    "revision": "4de97dd75164ccc8f9c8",
+    "url": "/dax_beauty/static/js/runtime-main.b4a26c4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dax_beauty/static/media/logo.5d5d9eef.svg"
+  }
+]);
